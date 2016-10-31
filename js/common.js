f64c646335d57fe3f3d3d8cbcb247c9200824a4b
Moik <turangr@gmail.com>
2016-10-31 23:30:16 +0200

    Gallery added

--- js/common.js
+++ js/common.js
@@ -28,20 +28,50 @@ $(function() {
 		onInit: fillSlideInfo,
 		onSlideChangeStart: fillSlideInfo
 
+	});
+
+    //gallery
+    $('.grid').masonry({
+    	// options
+    	itemSelector: '.grid-item'
     });
+
+	//popup
+	$('.popup-gallery').magnificPopup({
+		delegate: '.popup-slide',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-with-zoom',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Anna Ptah</small>';
+			}
+		},
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out' // CSS transition easing functio
+		}
+	});
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
 });
 
 $(window).on("load", function(){
-    $(".preloader").fadeOut();
+	$(".preloader").fadeOut();
 });
 
 function fillSlideInfo() {
 	var slide = $(".swiper-slide-active .slide-content"),
-		slideTitle = slide.data("title"),
-		slideDescr = slide.data("descr");
+	slideTitle = slide.data("title"),
+	slideDescr = slide.data("descr");
 
 	if(slideTitle != '') {
 		$(".slide-header").text(slideTitle);
